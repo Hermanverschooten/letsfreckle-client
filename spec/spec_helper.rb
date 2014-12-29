0a6a6bb1c2e3c4049ac045e8d9bd7a79242350a6
Herman verschooten <Herman@verschooten.net>
2014-12-29 12:54:54 +0100

    Move configure to helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,9 @@
 require './lib/letsfreckle/client'
+require 'pry'
+
+def setup_connection
+  Letsfreckle::Client.configure do
+    token 'scbp72wdc528hm8n52fowkma321tn58-jc1l2dkil0pnb75xjni48ad2wwsgr1d'
+    name 'letsfreckle-client'
+  end
+end
