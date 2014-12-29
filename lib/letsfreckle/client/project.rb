bad261fce4e3990b1a2abb8a06880d27e8bf9fcf
Herman verschooten <Herman@verschooten.net>
2014-12-29 12:55:18 +0100

    Find project

---
+++ lib/letsfreckle/client/project.rb
@@ -0,0 +1,45 @@
+module Letsfreckle
+  module Client
+    class NoProject;end
+    class Project
+      attr_reader :expenses_url, :uninvoiced_minutes, :url,
+        :expenses, :participants, :updated_at, :entries_url, :entries,
+        :created_at, :remaining_minutes, :billable_minutes, :enabled,
+        :invoiced_minutes, :minutes, :id, :invoices, :unbillable_minutes,
+        :budgeted_minutes, :billing_increment, :group, :color, :billable
+      attr_accessor :name
+
+      def load(hash)
+        hash.each do |k,v|
+          instance_variable_set("@#{k}".to_sym, v)
+        end
+      end
+
+      def self.find(id)
+        response = Letsfreckle::Client.read("/projects/#{id}")
+        if response.success?
+          p = Project.new
+          p.load response
+          return p
+        else
+          NoProject.new
+        end
+      end
+
+      def self.find_by(options)
+        response = Letsfreckle::Client.read("/projects", options)
+        if response.success?
+          projects = []
+          response.each do |proj|
+            p = Project.new
+            p.load proj
+            projects << p
+          end
+          return projects
+        else
+          []
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/letsfreckle/client/project_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe Letsfreckle::Client::Project do
+  let(:json_hash) {
+    {"expenses_url"=>"https://api.letsfreckle.com/v2/projects/250051/expenses",
+     "uninvoiced_minutes"=>0,
+     "url"=>"https://api.letsfreckle.com/v2/projects/250051",
+     "name"=>"test create",
+     "expenses"=>0,
+     "participants"=>[],
+     "updated_at"=>"2014-11-18T01:08:58Z",
+     "entries_url"=>"https://api.letsfreckle.com/v2/projects/250051/entries",
+     "entries"=>0,
+     "created_at"=>"2014-11-18T01:08:58Z",
+     "remaining_minutes"=>nil,
+     "billable_minutes"=>0,
+     "enabled"=>true,
+     "invoiced_minutes"=>0,
+     "minutes"=>0,
+     "id"=>250051,
+     "invoices"=>[],
+     "unbillable_minutes"=>0,
+     "budgeted_minutes"=>nil,
+     "billing_increment"=>15,
+     "group"=>nil,
+     "color"=>"#ef5555",
+     "billable"=>true}
+  }
+  it 'can be set from a hash' do
+    subject.load(json_hash)
+    expect(subject.name).to eql 'test create'
+  end
+
+  context 'connected' do
+    before do
+      setup_connection
+    end
+    it 'finds the project by its id' do
+      project = subject.class.find(250051)
+      expect(project).to be_a Letsfreckle::Client::Project
+    end
+    it 'returns a NoProject if not found' do
+      expect(subject.class.find(-1)).to be_a Letsfreckle::Client::NoProject
+    end
+    it 'finds projects by a given filter' do
+      expect(subject.class.find_by(name: 'test create').first).to be_a Letsfreckle::Client::Project
+    end
+    it 'returns an empty array if no projects match' do
+      expect(subject.class.find_by(name: 'some probably non-existing project name')).to eql([])
+    end
+  end
+end
