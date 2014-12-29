0860268778984a3e8575ec7fc47cea2371f1a042
Herman verschooten <Herman@verschooten.net>
2014-12-29 12:54:38 +0100

    Add read/write

--- lib/letsfreckle/client.rb
+++ lib/letsfreckle/client.rb
@@ -1,16 +1,42 @@
 require "letsfreckle/client/version"
 require "letsfreckle/client/configuration"
-require 'pry'
+require "letsfreckle/client/project"
+require "httparty"
 
 module Letsfreckle
   module Client
     extend self
+    include HTTParty
+
     attr_reader :configuration
 
-    def self.configure(&block)
+    base_uri 'https://api.letsfreckle.com/v2'
+
+    def configure(&block)
       raise 'configuration block required' unless block
       @configuration = Configuration.new
       configuration.instance_eval(&block)
+    end
+
+    def read(command, options={})
+      response = get(
+        command,
+        query: options,
+        headers: {
+          'User-Agent'  => configuration.name,
+          'X-FreckleToken'  => configuration.token
+        }
+      )
+    end
+    def write(command, options={})
+      response = post(
+        command,
+        query: options,
+        headers: {
+          'User-Agent'  => configuration.name,
+          'X-FreckleToken'  => configuration.token
+        }
+      )
     end
   end
 end
--- spec/lib/letsfreckle/client_spec.rb
+++ spec/lib/letsfreckle/client_spec.rb
@@ -9,4 +9,21 @@ describe Letsfreckle::Client do
     expect(Letsfreckle::Client.configuration.name).to eql('my name')
     expect(Letsfreckle::Client.configuration.token).to eql('token')
   end
+
+  context 'connected' do
+    before do
+      setup_connection
+    end
+    it 'reads the entries' do
+      response = subject.read('/projects')
+      expect(response.success?).to eql(true)
+      expect(response.count).to be > 0
+    end
+    it 'reads entries with a given filter' do
+      response = subject.read('/entries', billable: true)
+      expect(response.success?).to eql(true)
+      puts response.count
+      expect(response.count).to be > 0
+    end
+  end
 end
